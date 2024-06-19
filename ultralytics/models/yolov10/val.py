4e7f4022b01dc828060cbe9647de26bc64b68d83
BTZN0325 <sunjiahui@boton-tech.com>
2024-06-19 14:51:19 +0800

    update val(conf filter)

--- ultralytics/models/yolov10/val.py
+++ ultralytics/models/yolov10/val.py
@@ -16,9 +16,25 @@ class YOLOv10DetectionValidator(DetectionValidator):
         
         # Acknowledgement: Thanks to sanha9999 in #190 and #181!
         if preds.shape[-1] == 6:
-            return preds
+            # 如果conf是默认值0.001，那么就返回所有结果
+            if self.args.conf == 0.001:
+                return preds
+            else:
+                mask = preds[..., 4] > self.args.conf
+                if self.args.classes is not None:
+                    mask = mask & (preds[..., 5:6] == torch.tensor(self.args.classes, device=preds.device).unsqueeze(0)).any(2)
+                return [p[mask[idx]] for idx, p in enumerate(preds)]
         else:
             preds = preds.transpose(-1, -2)
             boxes, scores, labels = ops.v10postprocess(preds, self.args.max_det, self.nc)
             bboxes = ops.xywh2xyxy(boxes)
-            return torch.cat([bboxes, scores.unsqueeze(-1), labels.unsqueeze(-1)], dim=-1)
+            if self.args.conf == 0.001:
+                return torch.cat([bboxes, scores.unsqueeze(-1), labels.unsqueeze(-1)], dim=-1)
+            else:
+                # 如果conf是默认值0.001，那么就返回所有结果
+                preds = torch.cat([bboxes, scores.unsqueeze(-1), labels.unsqueeze(-1)], dim=-1)
+                mask = preds[..., 4] > self.args.conf
+                if self.args.classes is not None:
+                    mask = mask & (preds[..., 5:6] == torch.tensor(self.args.classes, device=preds.device).unsqueeze(0)).any(2)
+
+                return [p[mask[idx]] for idx, p in enumerate(preds)]
